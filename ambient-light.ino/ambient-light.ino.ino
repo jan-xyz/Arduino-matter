be40a7f3192c0c74fb4b8806777ff7bfa2ffbb76
Jan Steinke <me@janxyz.de>
2025-03-19 00:13:01 +0100

    add humidity

--- ambient-light.ino/ambient-light.ino.ino
+++ ambient-light.ino/ambient-light.ino.ino
@@ -1,5 +1,6 @@
 #include <Matter.h>
 #include <MatterIlluminance.h>
+#include <MatterHumidity.h>
 
 // If there's no built-in button set a pin where a button is connected
 #ifndef BTN_BUILTIN
@@ -7,8 +8,12 @@
 #endif
 
 MatterIlluminance matter_illuminance_sensor;
+MatterHumidity matter_humidity_sensor;
 
 int ambientLightPin = PA4;
+
+int soilPin = PD0;
+int soilPower = PD1;
 
 const float voltage = 3.3;
 const float adcMax = 4095.0;
@@ -20,9 +25,21 @@ void setup() {
 
   // Start Matter
   Matter.begin();
-  matter_illuminance_sensor.begin();
+
+  // Setup Reset Button and Satus LED
   pinMode(BTN_BUILTIN, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // Illuminance Setup
+  matter_illuminance_sensor.begin();
+
+  // Humidity Setup
+  matter_humidity_sensor.begin();
+  // Control the electricity flow to the sensor and set to LOW
+  // to increase longevity and reduce corrosion.
+  pinMode(soilPower, OUTPUT);
+  digitalWrite(soilPower, LOW);
+
 
   // Check commissioning
   if (!Matter.isDeviceCommissioned()) {
@@ -45,7 +62,7 @@ void setup() {
 
   // Wait for device to come online
   Serial.println("Waiting for Matter device discovery...");
-  while (!matter_illuminance_sensor.is_online()) {
+  while (!matter_illuminance_sensor.is_online() || !matter_humidity_sensor.is_online()) {
     decommission_handler();
     delay(200);
   }
@@ -57,20 +74,47 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  // Handle illuminance values
   static float current_illuminance_lux = 0.0f;
   static uint32_t last_action = 0;
   // Wait 10 seconds
-  if ((last_action + 10000) < millis()) {
+  if ((last_action + 30000) < millis()) {
     last_action = millis();
     float analogLight = analogRead(ambientLightPin);
     current_illuminance_lux = analogToLux(analogLight);
-    // Publish the illuminance value - you can also use 'matter_illuminance_sensor = current_illuminance_lux'
+    // Publish the illuminance value
     matter_illuminance_sensor.set_measured_value_lux(current_illuminance_lux);
     Serial.printf("Current light level: %.02f lx\n", current_illuminance_lux);
   }
 
+  // Handle humidity values
+  static float current_humidity = 0.0f;
+  static uint32_t last_action_humidity = 0;
+  // Wait 10 seconds
+  if ((last_action_humidity + 30000) < millis()) {
+    last_action_humidity = millis();
+    float analog_humidity = readSoil();
+    current_humidity = analogToPercent(analog_humidity);
+    // Publish the humidity value
+    matter_humidity_sensor.set_measured_value(current_humidity);
+    Serial.printf("Current humidity: %.01f%%\n", current_humidity);
+  }
+
   // Handle the decommissioning process if requested
   decommission_handler();
+}
+
+int analogToPercent(int analogValue) {
+  float percent = analogValue / voltage / 10;
+  return percent;
+}
+
+int readSoil() {
+  digitalWrite(soilPower, HIGH);  //turn D7 "On"
+  delay(10);                      //wait 10 milliseconds
+  float val = analogRead(soilPin);      //Read the SIG value form sensor
+  digitalWrite(soilPower, LOW);   //turn D7 "Off"
+  return val;                     //send current moisture value
 }
 
 int analogToLux(int analogValue) {
